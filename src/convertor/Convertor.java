9563e2c075dcc999298b2c6138861e213d857f9a
anna67 <ananyadey067@gmail.com>
2016-01-07 22:37:09 -0800

    Currency Converter
    Java

---
+++ src/convertor/Convertor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package convertor;
+
+/**
+ *
+ * @author Ayon
+ */
+public class Convertor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        list li = new list();
+        li.setVisible(true);
+        
+        
+    }
+}
